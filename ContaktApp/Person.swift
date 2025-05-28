dca53838ebb9a40caa95eacd807bd1bf92bda774
padla2023@gmail.com <padla2023@gmail.com>
2025-05-28 12:44:48 -0700

    add files

--- ContaktApp/Person.swift
+++ ContaktApp/Person.swift
@@ -5,4 +5,51 @@
 //  Created by ДОБРО on 22.05.2025.
 //
 
-import Foundation
+struct Person{
+    let firstName = ["Ivan", "John", "David", "Tim",
+                     "Vasya", "Mahmyd", "Abram",
+                     "Ben", "Alex", "Bruse"]
+    
+    let lastName = ["Johnson", "Ivanov", "Abrams", "Cook",
+                    "Petrov", "Maxudov", "Chen",
+                    "Kats", "Chaplin","Libman"]
+    
+    let numberImage = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
+    let numberPhone = ["80951112323", "80952223443",
+                       "80953332445", "8095556677",
+                       "80956578896", "80954445678",
+                       "80958789905", "80957869098",
+                       "80959097732", "80957770987"]
+    
+    let email = ["aaazzz@gmail.com", "pervy@gmail.com",
+                 "winter25@gmail.com", "spring23@gmail.com",
+                 "summer24@gmail.com", "yellow21@gmail.com",
+                 "green99@gmail.com", "red37@gmail.com",
+                 "yalta87@gmail.com", "blue69@gmail.com"]
+        
+    
+    
+    
+    var title: String{
+        "\(String(_: firstName.randomElement() ?? "1"))  \(String(_: lastName.randomElement() ?? "1"))"
+    }
+    
+
+    static func getPersonList() -> [Person]
+    {
+    [ Person(),
+      Person(),
+      Person(),
+      Person(),
+      Person(),
+      Person(),
+      Person(),
+      Person(),
+      Person(),
+      Person()
+      
+    ]
+   }
+ 
+}
--- PersonDetailsViewController.swift
+++ PersonDetailsViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class PersonDetailsViewController: UIViewController {
 
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabels: UILabel!
+    @IBOutlet var phoneLabels: UILabel!
+    @IBOutlet var emailLabels: UILabel!
+    
+    
+    
+    var person: Person!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        avatarImageView.image = UIImage(named: person.numberImage.randomElement() ?? "1")
+        nameLabels.text = person.title
+        phoneLabels.text = person.numberPhone.randomElement()
+        emailLabels.text = person.email.randomElement()
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
